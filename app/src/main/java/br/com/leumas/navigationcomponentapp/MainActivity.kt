84b10bbf78c80487e11cd9576a14dad2deb4fed2
Samuel01111 <samuel.nascimento.2015@hotmail.com>
2022-12-01 00:21:41 -0300

    master: class 4 - set custom action bar for dark and light theme.

--- app/src/main/java/br/com/leumas/navigationcomponentapp/MainActivity.kt
+++ app/src/main/java/br/com/leumas/navigationcomponentapp/MainActivity.kt
@@ -2,11 +2,34 @@ package br.com.leumas.navigationcomponentapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(myToolbar)
+
+        // responsaveis pela navegação entre as telas.
+        navController = findNavController(R.id.nav_host_fragment)
+        //Faz o gerenciamento de quem vai estar mostrando dependendo do current Fragment.
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    //onSupportNavigateUp = serve para respeitar o pilha do backstack do navController
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || return super.onSupportNavigateUp() // usando esse super pra voltar entre activitys tambem
     }
 }
